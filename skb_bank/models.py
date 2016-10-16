560adb1299afe716a24615cbea73475a19f3ec46
vdragan <vdragan1993@gmail.com>
2016-10-16 05:42:20 +0200

    Introduced model.

--- skb_bank/models.py
+++ skb_bank/models.py
@@ -1,4 +1,14 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
+
+class Clients(models.Model):
+    age = models.IntegerField()
+    bank = models.IntegerField()
+    buy_insurance = models.IntegerField()
+    citizenship = models.IntegerField()
+    income = models.IntegerField()
+    insurance = models.IntegerField()
+    intention = models.IntegerField()
+    interest = models.IntegerField()
+    type = models.IntegerField()
+    urgency = models.IntegerField()
